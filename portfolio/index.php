d22c197c0f1f2503c45998a61707b5862f689ae0
Ezequiel Bernal <ezebernal.dev@gmail.com>
2024-02-15 15:33:02 -0300

    Actualización de datos
    - Nuevo formato de CV.
    - Proyecto añadido (Campus virtual web).

---
+++ portfolio/index.php
@@ -0,0 +1,418 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Ezequiel Bernal</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" rel="stylesheet">
+
+</head>
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <span class="dynamic-text" id="typed-text"></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <!-- Reemplaza el ícono con tu propio elemento HTML -->
+                <span class="custom-toggler-element">&#9776;</span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">Acerca de mí</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#habilidades">Habilidades</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#proyectos">Proyectos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Inicio Section -->
+    <section id="about">
+        <div class="container">
+            <div id="txt-inicio">
+                <h3>Hola<span>,</span> Mi nombre es</h3>
+                <h1><span>Ezequiel Bernal</span>.</h1>
+                <h2>Desarrollador Web FullStack<span>.</span></h2>
+                <br>
+                <br>
+                <p>Técnico Universitario en Programación, me destaco en el desarrollo integral de sitios Web personalizados y aplicaciones de escritorio .NET<span>.</span>
+                </p>
+
+                <div class="social-icons">
+                    <a href="https://www.instagram.com/eze.bernal" target="_blank" class="social-icon"><i class="fab fa-instagram"></i></a>
+                    <a href="https://github.com/Ezeqfb" target="_blank" class="social-icon"><i class="fab fa-github"></i></a>
+                    <a href="https://www.linkedin.com/in/ezebernal/" target="_blank" class="social-icon"><i class="fab fa-linkedin"></i></a>
+                    <a href="CV-Ezequiel-Bernal.pdf" target="_blank" class="social-icon resume-button">
+                    Descargar CV
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Descripción Section -->
+    <br><br><br><br><br><br>
+    <section id="descripcion">
+        <div class="container">
+            <div data-aos="fade-down"
+                data-aos-easing="linear"
+                data-aos-duration="1050" id="txt-descripcion">
+                <h2>Descripción<span>.</span></h2>
+                <p>De antemano, agradezco tu visita a mi sitio, soy Ezequiel Bernal, vivo en Buenos Aires, Argentina. Como desarrollador estoy capacitado para realizar sitios web y aplicaciones personalizadas. Realizo pruebas exhaustivas de las funcionalidades, garantizando la solución de problemas y actualizaciones para asegurar el cumplimiento de los requerimientos del cliente. Trabajo como servicio técnico de PC y celulares desde los 16 años por pasión y hobby, fue mi primer paso para estudiar programación y decidir que este es mi camino. Estoy activamente en búsqueda de un puesto en IT que me permita ampliar mis conocimientos, mejorar mis habilidades y demostrar de lo que soy capaz como desarrollador.</p>
+            </div>
+            <br><br><br><br><br><br>
+        </div>
+    </section>
+
+
+
+
+    <!-- Experiencia Académica Section -->
+    <section id="experiencia">
+        <br>
+        <br>
+        <br>
+        <br>
+        <h2><span>Experiencia Académica</span></h2>
+        <p>Mi carrera y cursos</p>
+        <img src="img/book.png" alt="" id="libro">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <div class="container">
+
+            <div class="timeline"></div>
+
+            <!-- Tarjeta 1 -->
+            <div class="tarjeta-left">
+                <div class="info-tarjeta">
+                    <div data-aos="fade-right"
+                        data-aos-offset="300"
+                        data-aos-easing="ease-in-sine" class="tarjeta-fade">
+                        <H3>Curso: DevOps Engineer Core</H3>
+                        <h4>Universidad Tecmilenio x Digital NAO</h4>
+                        <p>Modalidad remota - Duración 6 meses</p>
+                        <br>
+                        <li>Administración de sistemas; Aprovisionamiento y la gestión de servidores e implementación de bases de datos.</li>
+                        <li>Supervisión de seguridad, aplicación de parches en sistemas.</li>
+                        <li>Gestión de la conectividad de red interna y externa.</li>
+                        <img src="img/flecha.png" alt="">
+                    </div>
+                </div>
+                
+                <div data-aos="zoom-in" data-aos-offset="370" class="point" id="point1"></div>
+                <div class="fechas">
+                    <div data-aos="fade-left"
+                        data-aos-offset="300"
+                        data-aos-easing="ease-in-sine" class="fecha-right">
+                        <H5>Nov 2023 - En curso</H5>
+                    </div>
+                </div>
+            </div>
+
+            <br>
+            <br>
+            <br>
+
+            <!-- Tarjeta 2 -->
+            <div class="tarjeta-right">
+                <div class="fechas">
+                    <div data-aos="fade-right"
+                        data-aos-offset="300"
+                        data-aos-easing="ease-in-sine" class="fecha-left">
+                        <H5>Feb 2022 - Dic 2023</H5>    
+                    </div>
+                </div>
+                <div data-aos="zoom-in" data-aos-offset="370" class="point" id="point2"></div>
+                <div class="info-tarjeta">
+                    <div data-aos="fade-left"
+                        data-aos-offset="300"
+                        data-aos-easing="ease-in-sine" class="tarjeta-fade">
+                        <H3>Tecnicatura Universitaria en Programación</H3>
+                        <h4>UTN FRA (Universidad Tecnológica Nacional)</h4>
+                        <p>Modalidad presencial - Duración 2 años</p>
+                        <br>
+                        <p>Aprendizaje destacado:</p>
+                        <li>Fuerte preparación en el pensamiento lógico y matemático. Fundamentos y lógica de programación.</li>
+                        <li>Desarrollo de Software, aplicaciones y sitios Web con bases de datos. Mantenimiento, testeo y actualización del software.</li>
+                        <li>Diseño y administración de bases de datos, diseño de software y documentación.</li>
+                        <br>
+                        <p>Estudios finalizados.</p>
+                        <img src="img/flecha.png" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <br>
+            <br>
+            <br>
+
+            <!-- Tarjeta 3 -->
+            <div class="tarjeta-left">
+                <div class="info-tarjeta">
+                    <div data-aos="fade-right"
+                        data-aos-offset="300"
+                        data-aos-easing="ease-in-sine" class="tarjeta-fade">
+                        <H3>Diplomatura en Desarrollo Web FullStack</H3>
+                        <h4>UTN FRRe (Universidad Tecnológica Nacional)</h4>
+                        <p>Modalidad remota - Duración (210 hs) 7 meses</p>
+                        <br>
+                        <li>FrontEnd: Diseño responsive, lenguajes HTML, CSS, JavaScript, Bootstrap.</li>
+                        <li>BackEnd: Bases de datos y comunicación, programación orientada a objetos, mapeo objeto-relacional, MySQL, SQL.</li>
+                        <img src="img/flecha.png" alt="">
+                    </div>
+                </div>
+                <div data-aos="zoom-in" data-aos-offset="370" class="point" id="point3"></div>
+                <div class="fechas">
+                    <div data-aos="fade-left"
+                        data-aos-offset="300"
+                        data-aos-easing="ease-in-sine" class="fecha-right">
+                        <H5>May - Nov 2021</H5>
+                    </div>
+                </div>
+            </div>
+
+            <br>
+            <br>
+            <br>
+
+            <!-- Tarjeta 4 -->
+            <div class="tarjeta-right">
+                <div class="fechas">
+                    <div data-aos="fade-right"
+                        data-aos-offset="300"
+                        data-aos-easing="ease-in-sine" class="fecha-left">
+                        <H5>Oct - Nov 2020</H5>
+                    </div>
+                </div>
+                <div data-aos="zoom-in" data-aos-offset="370" class="point" id="point4"></div>
+                <div class="info-tarjeta">
+                    <div data-aos="fade-left"
+                        data-aos-offset="300"
+                        data-aos-easing="ease-in-sine" class="tarjeta-fade">
+                        
+                        <H3>Curso: Programación Web</H3>
+                        <h4>Junior Achievement y Microsoft Argentina</h4>
+                        <p>Modalidad remota - Duración 2 meses</p>
+                        <br>
+                        <li>Diseño web básico con HTML, CSS y JavaScript, breve uso de Bootstrap.</li>
+                        <li>Ejercicios de lógica de programación con JavaScript.</li>
+                        <img src="img/flecha.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Habilidades Section -->
+    <br><br><br><br><br><br>
+    <section id="habilidades">
+        <div class="container">
+            <div data-aos="fade-up"
+                data-aos-duration="800" id="txt-descripcion">
+                <h2>Habilidades<span>.</span></h2>
+                <p>Estos son los lenguajes que más me han acompañado en mi camino como desarrollador. Mis favoritos.</p>
+            </div>
+            <br><br><br>
+            <div id="lenguajes" class="d-flex flex-wrap justify-content-center">
+                <div data-aos="fade-up"
+                    data-aos-duration="1000">
+                    <img src="img/html5.png" alt="">
+                </div>
+                <div data-aos="fade-up"
+                    data-aos-duration="1150">
+                    <img src="img/css3.png" alt="">
+                </div>
+                <!--<div data-aos="fade-up"
+                    data-aos-duration="1150">
+                    <img src="img/sass.png" alt="">
+                </div>-->
+                <div data-aos="fade-up"
+                    data-aos-duration="1300">
+                    <img src="img/js.png" alt="">
+                </div>
+                <!--<div data-aos="fade-up"
+                    data-aos-duration="1150">
+                    <img src="img/reactjs.png" alt="">
+                </div>-->
+                <div data-aos="fade-up"
+                    data-aos-duration="1450">
+                    <img src="img/bootstrap.png" alt="">
+                </div>
+                <!--<div data-aos="fade-up"
+                    data-aos-duration="2350">
+                    <img src="img/photoshop.png" alt="">
+                </div>-->
+                <div data-aos="fade-up"
+                    data-aos-duration="1600">
+                    <img src="img/php.png" alt="">
+                </div>
+                <div data-aos="fade-up"
+                    data-aos-duration="1750">
+                    <img src="img/mysql.png" alt="">
+                </div>
+                <!--<div data-aos="fade-up"
+                    data-aos-duration="1900">
+                    <img src="img/python.png" alt="">
+                </div>-->
+                <!--<div data-aos="fade-up"
+                    data-aos-duration="1900">
+                    <img src="img/c.png" alt="">
+                </div>-->
+                <div data-aos="fade-up"
+                    data-aos-duration="2050">
+                    <img src="img/c-sharp.png" alt="">
+                </div>
+                <div data-aos="fade-up"
+                    data-aos-duration="2200">
+                    <img src="img/net.png" alt="">
+                </div>
+                <!--<div data-aos="fade-up"
+                    data-aos-duration="2350">
+                    <img src="img/visualstudio.png" alt="">
+                </div>-->
+            </div>
+            <br><br><br><br><br><br>
+        </div>
+    </section>
+
+    <!-- Proyectos Section -->
+    <br><br><br><br><br><br>
+    <section id="proyectos">
+        <div class="container">
+            <div data-aos="fade-down"
+                data-aos-easing="linear"
+                data-aos-duration="1050" id="txt-descripcion">
+                <h2>Proyectos<span>.</span></h2>
+                <p>
+                    Los siguientes proyectos muestran mis habilidades y experiencia a través de ejemplos de 
+                    casos específicos. Cada proyecto contiene enlaces a repositorios de 
+                    código en donde puedes verlos con detalle y seguir mi progreso actual. El objetivo es 
+                    reflejar mi capacidad para resolver problemas complejos, trabajando con diferentes 
+                    tecnologías y gestionar proyectos solitarios o grupales de forma eficaz.
+                </p>
+            </div>
+            <div class="row mt-5 justify-content-center">
+                <!-- Tarjeta 1 -->
+                <div class="col-lg-4 mb-4" data-aos="fade-up" data-aos-duration="1000">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="proyecto-img">
+                                <img src="img/torneo.png" class="img-fluid" alt="Proyecto 1">
+                            </div>
+                            <h5 class="card-title">Sistema Gestor de Torneo Deportivo</h5>
+                            <p class="card-text">
+                                Sitio web de torneos personalizados temática taekwondo. Registra tu usuario
+                                para crear tu torneo, ingresa escuelas y oponentes para obtener un resultado final
+                                aleatorio.
+                                <br><br>
+                                <span>#PHP #MySQL #ORM #POO</span>
+                            </p>
+                            <a href="https://github.com/Ezeqfb/taekwondo-tournament" class="btn btn-primary">Ver Proyecto</a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <!-- Tarjeta 2 -->
+                <div class="col-lg-4 mb-4" data-aos="fade-up" data-aos-duration="1150">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="proyecto-img">
+                                <img src="img/campusweb.png" class="img-fluid" alt="Proyecto 2">
+                            </div>
+                            <h5 class="card-title">Campus virtual Web</h5>
+                            <p class="card-text">
+                                <br><br>
+                                Campus universitario para alumnos y profesores, sitio en donde compartir tareas, 
+                                avisos y un vistazo general de su situación en la institución para cada individuo.
+                                <br><br>
+                                <span>#MySQL #PHP</span>
+                            </p>
+                            <a href="https://github.com/Ezeqfb/campus-universitario" class="btn btn-primary">Ver Proyecto</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Tarjeta 3 -->
+                <div class="col-lg-4 mb-4" data-aos="fade-up" data-aos-duration="1300">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="proyecto-img">
+                                <img src="img/campusnet.png" class="img-fluid" alt="Proyecto 1">
+                            </div>
+                            <h5 class="card-title">App Campus virtual .NET</h5>
+                            <p class="card-text">
+                                <span>(En proceso)</span>
+                                <br><br>
+                                Aplicación de escritorio para alumnos de una institución,
+                                vistazo de su estado académico y calificaciones.
+                                <br><br>
+                                <span>#C-Sharp #WPF #Framework.NET</span>
+                            </p>
+                            <a href="enlace-al-proyecto-3" class="btn btn-primary">Ver Proyecto</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br><br><br><br><br><br>
+        </div>
+    </section>
+
+
+    <!-- Contact Section -->
+    <section id="contact" class="py-5">
+        <div class="container">
+            <br><br><br>
+
+            <div data-aos="fade-down">
+                <h2>Ponte en contacto<span>.</span></h2>
+                <h3>Si algo te llamó la atención, házmelo saber.</h3>
+                <h3>Gracias por tu visita.</h3>
+                <br><br><br>
+            </div>
+
+            <div id="contact-form">
+                <!-- Botón con enlace "mailto" y un ícono -->
+                <a href="mailto:ezebernal.dev@gmail.com" class="email-button">
+                    <i class="fa fa-envelope"></i> ezebernal.dev@gmail.com
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="text-center py-3">
+        <p>&copy; 2024 Desarrollado por Ezequiel Bernal</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+    <script src="js/load-ontop.js"></script>
+    <script src="js/nav-scroll.js"></script>
+    <script src="js/nav-logo-anim.js"></script>
+    <script>
+        AOS.init();
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
